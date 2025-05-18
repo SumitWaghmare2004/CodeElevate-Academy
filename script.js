3b61af7df468d06fb8cfc7475a2668d276ad1fc0
SumitWaghmare2004 <businesschavan2004@gmail.com>
2025-05-18 17:51:06 +0530

    1st Commit of CodeElevate Academy Website

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  setTimeout(() => {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('mainContent').style.display = 'block';
+  }, 3500);
+};
